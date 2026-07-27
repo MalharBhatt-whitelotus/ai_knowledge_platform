eb6887535c0503e87eb648f69a1d3c9de773f677
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-27 15:32:02 +0530

    Refact: Enhance the shared  app factory system.

--- shared_lib/app/factory.py
+++ shared_lib/app/factory.py
@@ -1,13 +1,30 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 
 from shared_lib.config.settings import settings
+from shared_lib.logger.logger import get_logger
 
 def create_app(service_name: str, router: APIRouter) -> FastAPI:
     """
     Creata and configure a FastAPI Application.
     """
-    app = FastAPI(title=service_name, version=settings.APP_VERSION)
+
+    logger = get_logger(service_name)
+
+    @asynccontextmanager
+    async def lifespan(app: FastAPI):
+        logger.info(f"{service_name} starting...")
+        yield
+        logger.info(f"{service_name} shutting down...")
+
+    app = FastAPI(
+        title=service_name, 
+        version=settings.APP_VERSION, 
+        lifespan=lifespan
+        )
+
     app.include_router(router)
 
     return app
