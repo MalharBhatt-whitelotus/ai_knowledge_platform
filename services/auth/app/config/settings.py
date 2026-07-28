e3c0ea3062a158828dd57c128c0d342a7b36bad6
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-28 17:28:36 +0530

    Feat: Initial JWT token implementation.

--- services/auth/app/config/settings.py
+++ services/auth/app/config/settings.py
@@ -9,6 +9,12 @@ class AuthSettings(BaseSettings):
 
     DATABASE_URL: str
 
+    JWT_SECRET_KEY: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int =7
+    JWT_ALGORITHMS: str = "HS256"
+
+
     model_config = SettingsConfigDict(env_file=BASE_DIR/".env", extra="ignore")
 
 @lru_cache
---
+++ services/auth/app/security/jwt.py
@@ -0,0 +1,77 @@
+from datetime import UTC, datetime, timedelta
+from typing import Any
+
+from jose import JWTError, jwt
+
+from app.config.settings import settings
+
+class JWTManager:
+    """Utility class for creating and verifying JWT tokens."""
+
+    @staticmethod
+    def create_access_token(data: dict[str, Any]) -> str:
+        payload = data.copy()
+
+        now = datetime.copy()
+
+        payload.update(
+            {
+                "iat": now,
+                "exp": now + timedelta(
+                    minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES
+                ),
+                "type": "access",
+            }
+        )
+
+        return jwt.encode(
+            payload,
+            settings.JWT_SECRET_KEY,
+            algorithm=settings.JWT_ALGORITHMS,
+        )
+
+    @staticmethod
+    def create_refresh_token(data: dict[str, Any]) -> str:
+        payload = data.copy()
+
+        now = datetime.now(UTC)
+
+        payload.update(
+            {
+                "iat": now,
+                "exp": now + timedelta(days=settings.JWT_REFRESH_TOKEN_EXPIRE_DAYS),
+                "type": "refresh",
+            }
+        )
+
+        return jwt.encode(
+            payload,
+            settings.JWT_SECRET_KEY,
+            algorithm=settings.JWT_ALGORITHMS   
+        )
+
+    @staticmethod
+    def decode_token(token: str) -> dict[str, Any]:
+        return jwt.decode(
+            token,
+            settings.JWT_SECRET_KEY,
+            algorithms=[settings.JWT_ALGORITHMS]
+        )
+
+    @staticmethod
+    def verify_access_token(token: str) -> dict[str, Any]:
+        payload = JWTManager.decode_token(token)
+
+        if payload.get("type") != "access":
+            raise JWTError("Invalid token type.")
+
+        return payload
+
+    @staticmethod
+    def verify_refresh_token(token: str) -> dict[str, Any]:
+        payload = JWTManager.decode_token(token)
+
+        if payload.get("type") != "refresh":
+            raise JWTError("Invalid token type.")
+
+        return payload
