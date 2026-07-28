07d941a2afeade68d68dd32ef2a989b602836df1
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-28 10:40:29 +0530

    Initial Auth_Database implementation. :

---
+++ services/auth/database/auth_database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from auth.app.config.settings import settings
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True, pool_pre_ping=NullPool)
+
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
+
+Base = declarative_base()
+
+async def get_database():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        await db.close()
