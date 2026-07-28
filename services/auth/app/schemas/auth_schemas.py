03d54a46de7f8c1025dc4deba76d30a67ca54cd7
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-28 16:42:42 +0530

    Feat: Initial implementation of login routes.

--- services/auth/app/schemas/auth_schemas.py
+++ services/auth/app/schemas/auth_schemas.py
@@ -1,8 +1,35 @@
+from datetime import datetime
 from pydantic import BaseModel, Field
+
+
+class RegisterRequest(BaseModel):
+    first_name: str = Field(..., min_length=1, max_length=100)
+    last_tname: str = Field(..., min_length=1, max_length=100)
+    email: str = Field(..., min_length=5, max_length=100)
+    username: str = Field(..., min_length=3, max_length=100)
+    password: str = Field(..., min_length=6, max_length=15)
+    role: str = Field(default="user")
+    
+    
+class RegisterResponse(BaseModel):
+    id: int
+    uuid: str
+    first_name: str
+    last_name: str
+    email: str
+    username: str
+    password_hash: str
+    role: str
+    is_active: bool
+    is_verified: bool
+    created_at: datetime
+    updated_at: datetime
+
 
 class AuthRequest(BaseModel):
     username: str = Field(..., min_length=1, max_length=100)
     password: str = Field(..., min_length=6, max_length=15)
+
 
 class AuthResponse(BaseModel):
     username: str
--- services/auth/app/services/auth_service.py
+++ services/auth/app/services/auth_service.py
@@ -2,9 +2,14 @@ from fastapi import HTTPException, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from services.auth.app.repositories import auth_repository as repo
-from services.auth.app.schemas.auth_schemas import AuthRequest, AuthResponse
+from services.auth.app.schemas.auth_schemas import RegisterRequest, RegisterResponse, AuthRequest, AuthResponse
 
 class AuthService:
 
-    async def register_user(user_credentials: AuthRequest, db: AsyncSession) -> AuthResponse:
-        return AuthResponse
+
+    async def register_user(user_credentials: RegisterRequest, db: AsyncSession) -> RegisterResponse:
+        ...
+
+
+    async def login_user(user_credentials: AuthRequest, db: AsyncSession) -> AuthResponse:
+       ...
