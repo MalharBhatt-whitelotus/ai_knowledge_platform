40cdafc5f0fbd3c304bc78b01d997591d8c2c3c5
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-28 10:51:52 +0530

    Feat: Initial implementation of configuration.

---
+++ services/auth/app/config/settings.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+class AuthSettings(BaseSettings):
+
+    SERVICE_NAME: str
+
+    DATABASE_URL: str
+
+    model_config = SettingsConfigDict(env_file=BASE_DIR/".env", extra="ignore")
+
+@lru_cache
+def get_settings() -> AuthSettings:
+    return AuthSettings()
+
+settings =  get_settings()
