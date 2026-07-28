6dff3183a48cf804132cc9fca293fd815bedee0b
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-28 18:57:07 +0530

    Initial init file commits.

---
+++ services/auth/app/__init__.py
@@ -0,0 +1,71 @@
+"""
+Features We'll Build
+Sprint 1
+Feature 2.1
+
+✅ Project Setup
+
+Feature 2.2
+
+✅ Database Connection
+
+Feature 2.3
+users
+────────────────────────────────────────────
+
+id
+
+uuid
+
+first_name
+
+last_name
+
+email
+
+username
+
+password_hash
+
+role
+
+is_active
+
+is_verified
+
+created_at
+
+updated_at
+✅ User Model
+
+Feature 2.4
+
+✅ Register API
+
+Feature 2.5
+
+✅ Login API
+
+Feature 2.6
+
+✅ JWT
+
+Feature 2.7
+
+✅ Current User API
+
+Feature 2.8
+
+✅ Refresh Token
+
+Feature 2.9
+
+✅ Role Middleware
+
+Feature 2.10
+
+✅ Tests
+"""
+
+
+
