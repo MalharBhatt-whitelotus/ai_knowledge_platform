c67fa574a75f72a283ad7cd9b40668eea1eb2131
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-29 15:00:29 +0530

    Feat: Implement Utilitiy functions for login service.

--- services/auth/app/utils/auth_utils.py
+++ services/auth/app/utils/auth_utils.py
@@ -1,31 +1,58 @@
 import uuid
 from passlib.context import CryptContext
+
+
 class AuthUtils:
 
 
-    pwd_handler = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+   pwd_handler = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 
 
-    @staticmethod
-    def check_password(password: str) -> bool:
-       has_upper = any(c.isupper() for c in password)
-       has_lower = any(c.islower() for c in password)
-       has_digit = any(c.isdigit() for c in password)
-       has_special = any(not c.isalnum() for c in password)
+   """
+   ---------------------------------------
+     * Check Password Utility Function *
+   ---------------------------------------
+   """
+   @staticmethod
+   def check_password(password: str) -> bool:
+      has_upper = any(c.isupper() for c in password)
+      has_lower = any(c.islower() for c in password)
+      has_digit = any(c.isdigit() for c in password)
+      has_special = any(not c.isalnum() for c in password)
 
-       return has_upper and has_lower and has_digit and has_special
+      return has_upper and has_lower and has_digit and has_special
 
 
-    @staticmethod
-    def hash_password(password: str) -> str:
-       hashed_password = AuthUtils.pwd_handler.hash(password)
-       return hashed_password
+   """
+   ---------------------------------------
+     * Hash Password Utility Function *
+   ---------------------------------------
+   """
+   @staticmethod
+   def hash_password(password: str) -> str:
+      print(password)
+      hashed_password = AuthUtils.pwd_handler.hash(password)
+
+      return hashed_password
 
 
-    @staticmethod
-    def verify_password(password: str, hashed_password: str) -> bool:
-       return AuthUtils.pwd_handler.verify(password, hashed_password)
+   """
+   ---------------------------------------
+    * Verify Password Utility Function *
+   ---------------------------------------
+   """
+   @staticmethod
+   def verify_password(password: str, hashed_password: str) -> bool:
+      print(password)
+      print(hashed_password)
+      return AuthUtils.pwd_handler.verify(password, hashed_password)
 
-    @staticmethod
-    def get_uuid() -> str:
-       return uuid.uuid4()
+
+   """
+   ---------------------------------------
+        * Get UUID Utility Function *
+   ---------------------------------------
+   """
+   @staticmethod
+   def get_uuid() -> str:
+      return uuid.uuid4()
