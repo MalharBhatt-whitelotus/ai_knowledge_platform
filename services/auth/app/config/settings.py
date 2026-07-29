ec81a1f3eda4cb2fc88a3c64ed632d7714a75e1d
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-29 18:47:59 +0530

    Refact: Change the database_url and its components.

--- services/auth/app/config/settings.py
+++ services/auth/app/config/settings.py
@@ -7,13 +7,25 @@ class AuthSettings(BaseSettings):
 
     SERVICE_NAME: str
 
-    DATABASE_URL: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
 
     JWT_SECRET_KEY: str
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     JWT_REFRESH_TOKEN_EXPIRE_DAYS: int =7
     JWT_ALGORITHMS: str = "HS256"
 
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}"
+            f"/{self.POSTGRES_DB}"
+        )
 
     model_config = SettingsConfigDict(env_file=BASE_DIR/".env", extra="ignore")
 
@@ -22,3 +34,6 @@ def get_settings() -> AuthSettings:
     return AuthSettings()
 
 settings =  get_settings()
+print("BASE_DIR =", BASE_DIR)
+print("ENV FILE =", BASE_DIR / ".env")
+print("DATABASE_URL =", settings.DATABASE_URL)
