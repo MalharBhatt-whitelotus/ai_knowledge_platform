7dfd46b7094c1fbb818a045f6825e78ca9a86353
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-29 12:01:56 +0530

    Feat: Implemented utility funcitons:  - check_password()  - hash_password()  - get_uuid()  -  verify_password()

--- services/auth/app/utils/auth_utils.py
+++ services/auth/app/utils/auth_utils.py
@@ -1,12 +1,31 @@
+import uuid
+from passlib.context import CryptContext
 class AuthUtils:
-    @staticmethod
-    def hash_password(password):
-        ...
+
+
+    pwd_handler = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+
 
     @staticmethod
-    def check_email(email):
-        ...
+    def check_password(password: str) -> bool:
+       has_upper = any(c.isupper() for c in password)
+       has_lower = any(c.islower() for c in password)
+       has_digit = any(c.isdigit() for c in password)
+       has_special = any(not c.isalnum() for c in password)
+
+       return has_upper and has_lower and has_digit and has_special
+
 
     @staticmethod
-    def check_password(password):
-        ...
+    def hash_password(password: str) -> str:
+       hashed_password = AuthUtils.pwd_handler.hash(password)
+       return hashed_password
+
+
+    @staticmethod
+    def verify(password: str, hashed_password: str) -> bool:
+       return AuthUtils.pwd_handler.verify(password, hashed_password)
+
+    @staticmethod
+    def get_uuid() -> str:
+       return uuid.uuid4()
