808c8b7f69cb1f0fd1995fbd837697390a901cb6
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-30 17:23:22 +0530

    Refract: Change the role checker allowed values calling features.

--- services/auth/app/dependencies/role_checker.py
+++ services/auth/app/dependencies/role_checker.py
@@ -25,6 +25,7 @@ END CLASS
 """
 from fastapi import Depends, HTTPException, status
 
+from services.auth.app.models.auth_models import Role
 from services.auth.app.schemas.auth_schemas import CurrentUserResponse
 from services.auth.app.dependencies.current_user import get_current_user
 class RoleChecker:
@@ -48,6 +49,6 @@ class RoleChecker:
         except Exception as exc:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(exc))
 
-admin_only = RoleChecker(["admin"])
-user_only = RoleChecker(["user"])
-user_or_admin = RoleChecker(["user", "admin"])
+admin_only = RoleChecker([Role.admin.value])
+user_only = RoleChecker([Role.user.value])
+user_or_admin = RoleChecker([Role.user.value, Role.admin.value])
