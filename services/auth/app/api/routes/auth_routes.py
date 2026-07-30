1f8f9088e2d6691f5465e4926fec5287a966bd17
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-30 15:51:40 +0530

    Feat:  - Implement Current_user() route: "/me"  - Implement logout_user() route: "/logout_user"

--- services/auth/app/api/routes/auth_routes.py
+++ services/auth/app/api/routes/auth_routes.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, requests,status, Depends
+from fastapi.security import HTTPAuthorizationCredentials
 
 from shared_lib.config.settings import settings
 from shared_lib.schemas.health import HealthResponse
@@ -8,7 +9,8 @@ from shared_lib.schemas.health import HealthResponse
 from services.auth.app.database.auth_database import get_db
 from services.auth.app.config.settings import settings as auth_settings
 from services.auth.app.services.auth_service import AuthService as service
-from services.auth.app.schemas.auth_schemas import RegisterRequest,RegisterResponse, AuthRequest, AuthResponse
+from services.auth.app.dependencies.current_user import get_current_user
+from services.auth.app.schemas.auth_schemas import RegisterRequest,RegisterResponse, AuthRequest, AuthResponse, CurrentUserResponse, TokenResponse
 
 
 router = APIRouter()
@@ -25,11 +27,16 @@ async def register_user(user_credentials: RegisterRequest, db: AsyncSession = De
     return result
 
 
-@router.post("/login_user", response_model=AuthResponse)
+@router.post("/login_user", response_model=AuthResponse, status_code=200)
 async def login_user(user_credentials: AuthRequest, db: AsyncSession = Depends(get_db)):
     result = await service.login_user(user_credentials, db)
     return result
 
-@router.get("/me", response_model=RegisterResponse)
-async def current_user():
-    ...
+@router.post("/logout_user", response_model=AuthResponse, status_code=200)
+async def logout_user(user_credentials: AuthRequest, db: AsyncSession = Depends(get_db)):
+    result = await service.logout_user(user_credentials, db)
+    return result
+    
+@router.post("/me", response_model=CurrentUserResponse, status_code=200)
+async def current_user(current_user = Depends(get_current_user)):
+    return current_user
