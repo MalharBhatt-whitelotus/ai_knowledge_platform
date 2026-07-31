57f5738fe1583b4e8bf2143e862e1cf1b62a525d
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-31 12:45:52 +0530

    Feat: Implementation of download() abstractmethod in storage module / LocalStorage.

--- services/file/app/storage/local_storage.py
+++ services/file/app/storage/local_storage.py
@@ -5,6 +5,12 @@ from services.file.app.storage.storage_provider import StorageProvider
 
 class LocalStorage(StorageProvider):
 
+
+    """
+    -------------------------------------
+            * Init Function * 
+    -------------------------------------
+    """
     def __init__(self, upload_directory: str) -> None:
         self.upload_dir = Path(upload_directory)
         self.upload_dir.mkdir(parents=True, exist_ok=True)
@@ -26,7 +32,7 @@ class LocalStorage(StorageProvider):
 
     """
     -------------------------------------
-            * Save File Function * 
+            * Delete File Function * 
     -------------------------------------
     """
     async def delete(self, path: str) -> None:
@@ -36,3 +42,17 @@ class LocalStorage(StorageProvider):
             raise FileNotFoundError(path)
 
         file_path.unlink()
+
+
+    """
+    -------------------------------------
+            * Download File Function * 
+    -------------------------------------
+    """
+    async def download(self, path: str) -> bytes:
+        file_path = Path(path)
+
+        if not file_path.exists():
+            raise FileNotFoundError(path)
+
+        return file_path.read_bytes()
