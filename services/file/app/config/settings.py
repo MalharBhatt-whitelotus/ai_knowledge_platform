1c3a1a450e9ee0b49fd32ebddf381a621b47d710
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-31 10:43:57 +0530

    Feat: Initial implementation of file_config. Refract: Remove the print statement in auth_config. Refract: Change the env_file directory in the docker compose for file_service.

--- services/file/app/config/settings.py
+++ services/file/app/config/settings.py
@@ -1,22 +1,18 @@
 from pathlib import Path
 from functools import lru_cache
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import SettingsConfigDict, BaseSettings
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-class AuthSettings(BaseSettings):
+class FileSettings(BaseSettings):
 
     SERVICE_NAME: str
 
     POSTGRES_HOST: str
-    POSTGRES_PORT: int
+    POSTGRES_PORT: str
     POSTGRES_DB: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
-
-    JWT_SECRET_KEY: str
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int =7
-    JWT_ALGORITHMS: str = "HS256"
 
     @property
     def DATABASE_URL(self) -> str:
@@ -25,12 +21,15 @@ class AuthSettings(BaseSettings):
             f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
             f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}"
             f"/{self.POSTGRES_DB}"
-        )
+            )
 
     model_config = SettingsConfigDict(env_file=BASE_DIR/".env", extra="ignore")
 
 @lru_cache
-def get_settings() -> AuthSettings:
-    return AuthSettings()
+def get_settings() -> FileSettings:
+    return FileSettings()
 
-settings =  get_settings()
+settings = get_settings()
+print("BASE_DIR =", BASE_DIR)
+print("ENV FILE =", BASE_DIR / ".env")
+print("DATABASE_URL =", settings.DATABASE_URL)
