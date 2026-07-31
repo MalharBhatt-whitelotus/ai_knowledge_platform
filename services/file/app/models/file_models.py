0864b47680202891feb089292ea9d217b925cbdb
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-31 11:09:39 +0530

    Refract: Centralize the DocType and DocStatus enums in services/file/app/enums.py

--- services/file/app/models/file_models.py
+++ services/file/app/models/file_models.py
@@ -1,26 +1,13 @@
-import enum
 from sqlalchemy import Column, String, Integer, DateTime, Enum, Boolean
 
 from services.file.app.config.settings import settings
-
-
-class DocType(enum.Enum):
-    pdf = "pdf"
-    txt = "txt"
-    docx = "docx"
-    xlsx = "xlsx"
-    pptx = "pptx"
-
-
-class DocStatus(enum.Enum):
-    in_queue = "in_queue"
-    pending = "pending"
-    completed = "completed"
+from services.file.app.enums import DocType, DocStatus
 
 
 class File:
 
     id = Column(Integer, primary_key=True, index=True)
+    file_id = Column(String, unique=True, nullable=False, index=True)
     owner_id = Column(String, nullable=False, index=True)
     original_filename = Column(String, nullable=False, index=True)
     stored_filename = Column(String, nullable=False, index=True, unique=True)
