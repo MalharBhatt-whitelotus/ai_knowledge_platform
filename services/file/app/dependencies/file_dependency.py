03d7eec434afeb08096592f09fae8e4d102ed6b5
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-03 18:23:48 +0530

    Refract: Add publisher dependency for rabbitmq implementation in file uploading.

--- services/file/app/dependencies/file_dependency.py
+++ services/file/app/dependencies/file_dependency.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from fastapi import Request
 
 from services.file.app.storage.local_storage import LocalStorage
 from services.file.app.services.file_services import FileServices
@@ -8,10 +8,13 @@ from services.file.app.repositories.file_repository import FileRepository as rep
 
 directory = Path(__file__).resolve().parent.parent.parent
 
-def get_file_service():
+
+def get_file_service(request: Request):
     storage = LocalStorage(f"{directory}/saved_files")
     services = FileServices(
         repository=repo,
-        storage_provider=storage
+        storage_provider=storage,
+        publisher=request.app.state.publisher,
         )
+
     return services
