c4178e40b9688fa76f4fad27900d9844fc7a0fd9
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-04 12:59:55 +0530

    Feat:   - Implement get_file() routes: "/internal/get_file/{file_id}",   - Implement download_file() routes: "internal/download_file/{file_id}".    , for handle() in file_upload_handler.

--- services/file/app/api/routes/file_routes.py
+++ services/file/app/api/routes/file_routes.py
@@ -1,5 +1,7 @@
+from io import BytesIO
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter,status,UploadFile,File, Depends
+from fastapi.responses import StreamingResponse
 
 from services.file.app.database.file_database import get_db
 from services.file.app.dependencies.file_dependency import get_file_service
@@ -30,7 +32,7 @@ async def upload_file(
 
 @file_router.get("/get_file/{file_id}", response_model=FileResponse, status_code=status.HTTP_200_OK)
 async def get_file(
-    file_id: int, 
+    file_id: str, 
     db: AsyncSession = Depends(get_db),
     current_user = Depends(user_only),
     services: FileServices = Depends(get_file_service)):
@@ -38,3 +40,27 @@ async def get_file(
     file = await services.get_file_by_file_id(file_id, current_user.user_id, db)
 
     return file
+
+
+@file_router.get("/internal/get_file/{file_id}", response_model=FileResponse, status_code=status.HTTP_200_OK)
+async def get_file(
+    file_id: str,  
+    db: AsyncSession = Depends(get_db), 
+    services: FileServices = Depends(get_file_service)
+    ):
+
+    file = await services.get_internal_file(file_id, db)
+    
+    return file
+
+
+@file_router.get("/internal/download_file/{file_id}", response_class=StreamingResponse, status_code=status.HTTP_200_OK)
+async def download_file(
+    file_id: str,
+    db: AsyncSession = Depends(get_db),
+    services: FileServices = Depends(get_file_service),
+):
+    
+    file_bytes = await services.get_file_bytes(file_id, db)
+
+    return StreamingResponse(content= BytesIO(file_bytes))
