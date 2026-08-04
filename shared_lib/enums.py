781e442d5900a0e48cda19416d2399a504f6b437
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-04 12:57:47 +0530

    Feat: Implement DocType and DocStatus enums in shared_lib.

--- shared_lib/enums.py
+++ shared_lib/enums.py
@@ -3,3 +3,16 @@ import enum
 class Role(enum.Enum):
     user = "user"
     admin = "admin"
+
+class DocType(enum.Enum):
+    pdf = "pdf"
+    txt = "txt"
+    docx = "docx"
+    xlsx = "xlsx"
+    pptx = "pptx"
+
+
+class DocStatus(enum.Enum):
+    in_queue = "in_queue"
+    pending = "pending"
+    completed = "completed"
