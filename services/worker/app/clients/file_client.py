669e38910d1775576fe1f433be77753af312cc8e
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-04 13:02:02 +0530

    Feat: Implement download_file() in file_client. Refract: Refract get_file() in file_client.

--- services/worker/app/clients/file_client.py
+++ services/worker/app/clients/file_client.py
@@ -1,39 +1,52 @@
 import httpx
-from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import HTTPException, status
 
-
-security = HTTPBearer()
-
+from services.worker.app.schemas.file_client_schemas import FileResponse, FileDownloadResponse
 
 class FileClient:
 
 
     def __init__(self):
-        self.Base_url = "http://file:8002"
+        self.Base_url = "http://file_service:8002"
 
 
     async def get_file(
             self, 
-            file_id: str, 
-            authorization: HTTPAuthorizationCredentials = Depends(security)
-            ):
-        token = authorization.credentials
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
+            file_id: str,
+            ) -> FileResponse:
+        try: 
+            async with httpx.AsyncClient(timeout=10.0) as client:
+                response =  await client.get(
+                    f"{self.Base_url}/internal/get_file/{file_id}",
+                    )
 
-        async with httpx.AsyncClient as client:
-           response =  await client.get(f"{self.Base_url}/get_file/{file_id}", headers=headers)
-
-           if response.status_code != status.HTTP_200_OK:
-               raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+            if response.status_code != status.HTTP_200_OK:
+                raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+            
+            return FileResponse.model_validate(response.json())
         
-           return response
+        except httpx.RequestError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="File Service unavailable")
+
+        except HTTPException:
+            raise
 
 
-    async def download_file(self, file_id: str):
-        ...
+    async def download_file(self, file_id: str) -> bytes:
+        try:
+            async with httpx.AsyncClient(timeout=10.0) as client:
+                response = await client.get(f"{self.Base_url}/internal/download_file/{file_id}")
+
+                if response.status_code != status.HTTP_200_OK:
+                    raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+
+                return response.content
+            
+        except httpx.RequestError:
+            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="File service unavailable.")
+
+        except HTTPException:
+            raise
 
     async def update_status(self, file_id: str, status: str):
         ...
