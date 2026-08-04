1d32583e31f19ca4d3871a59787557d52acdf000
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-04 16:56:03 +0530

    Feat: Implementation of extract_text() in pdf_parser,py

--- services/file/app/parser/pdf_parser.py
+++ services/file/app/parser/pdf_parser.py
@@ -1,6 +1,15 @@
+import fitz
+
 from services.file.app.parser.base_parser import BaseParser
 
 class PdfParser(BaseParser):
 
     async def extract_text(self, file_path: str) -> str:
-        ...
+        pdf = fitz.open(file_path)
+        text = ""
+
+        for page in pdf:
+            text += page.get_text()
+
+        pdf.close()
+        return text
