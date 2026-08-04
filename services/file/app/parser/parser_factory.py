a2b247878cced508ea9b49c470dcbf5a323a7621
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-04 15:27:07 +0530

    Feat: ParserFactory Implementation for choosing suitable parser in accordance to the doctype.

---
+++ services/file/app/parser/parser_factory.py
@@ -0,0 +1,24 @@
+from services.file.app.parser.docx_parser import DocxParser
+from services.file.app.parser.pdf_parser import PdfParser
+from services.file.app.parser.pptx_parser import PptxParser
+from services.file.app.parser.txt_parser import TxtParser
+from services.file.app.parser.xlsx_parser import XlsxParser
+
+from shared_lib.enums import DocType
+
+class ParserFactory:
+
+    _parser = {
+        DocType.docx.value: DocxParser(),
+        DocType.pdf.value: PdfParser(),
+        DocType.pptx.value: PptxParser(),
+        DocType.txt.value: TxtParser(),
+        DocType.xlsx.value: XlsxParser(),
+    }
+
+    @classmethod
+    def get_parser(cls, content_type: str):
+        parser = cls._parser.get(content_type)
+        if not parser:
+            return None
+        return parser
