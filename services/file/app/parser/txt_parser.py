1168faddd274b0ab6cacd241fd9024f97bafcbbc
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-04 17:14:21 +0530

    Feat: Implement extract_text() text_parser.

--- services/file/app/parser/txt_parser.py
+++ services/file/app/parser/txt_parser.py
@@ -2,4 +2,6 @@ from services.file.app.parser.base_parser import BaseParser
 
 class TxtParser(BaseParser):
     async def extract_text(self, file_path: str) -> str:
-            ...
+        with open(file_path, "r", encoding="utf-8") as file:
+                text = file.read()
+        return text
