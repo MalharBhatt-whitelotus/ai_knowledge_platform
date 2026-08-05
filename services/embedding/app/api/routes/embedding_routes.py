473ab2c56399450cc4f8aa0fb6e25c6074089fa6
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-05 13:24:49 +0530

    Fix: Routing get_embeddings method.

--- services/embedding/app/api/routes/embedding_routes.py
+++ services/embedding/app/api/routes/embedding_routes.py
@@ -1,18 +1,14 @@
-from fastapi import APIRouter, Request, status, Depends
+from fastapi import APIRouter, status
 
-from services.embedding.app.services.embedding_services import EmbeddingService
+from services.embedding.app.services import embedding_service as services
 from services.embedding.app.schemas.embedding_schema import EmbeddingRequest, EmbeddingResponse
 
 embedding_router = APIRouter()
 
-def get_embedding_service() -> EmbeddingService:
-    return EmbeddingService()
-
 @embedding_router.post("/internal/generate_embeddings", response_model=EmbeddingResponse, status_code=status.HTTP_200_OK)
 async def generate_embeddings(
     request: EmbeddingRequest,
-    services: EmbeddingService = Depends(get_embedding_service)
     ):
-    embeddings = await services.generate_embeddings(request)
+    embeddings = await services.generate_embeddings(request.chunks)
 
     return EmbeddingResponse(embeddings=embeddings)
