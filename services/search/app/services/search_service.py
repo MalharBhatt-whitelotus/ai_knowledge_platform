e75c33a4cf62d8e7b326d0edc44ca62c4a2f9475
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-05 16:28:50 +0530

    Feat: Implement store_embeddings() method in search_service.

--- services/search/app/services/search_service.py
+++ services/search/app/services/search_service.py
@@ -1,7 +1,30 @@
 from fastapi import HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from services.search.app.schemas.search_schemas import StoreEmbeddingRequest
+from services.search.app.repositories.chroma_repository import ChromaRepository
+from services.search.app.schemas.search_schemas import StoreEmbeddingRequest, StoreEmbeddingResponse
+
 class SearchService:
-    async def store_embeddings(request: StoreEmbeddingRequest, db: AsyncSession):
-        ...
+    def __init__(self, repo: ChromaRepository) -> None:
+        self.repo = repo
+
+    async def store_embeddings(self, request: StoreEmbeddingRequest) -> StoreEmbeddingResponse:
+        try:
+            await self.repo.add(
+                file_id = request.file_id, 
+                owner_id=request.owner_id,
+                chunks=request.chunks,
+                embeddings=request.chunks
+                )
+
+            return StoreEmbeddingResponse(
+                success=True,
+                message="Embedding stored successfully.",
+                file_id=request.file_id,
+                chunks_stored=len(request.chunks)
+            )
+
+        except Exception as exc:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=f"Failed to store embeddings: {exc}",
+            )
