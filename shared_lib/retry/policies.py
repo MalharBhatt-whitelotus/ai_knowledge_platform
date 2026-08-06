bd1a13200ede029838306de05a4f5792415c0ec0
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 18:25:08 +0530

    Refract: Change the module import position in accordance to pep8.

--- shared_lib/retry/policies.py
+++ shared_lib/retry/policies.py
@@ -1,10 +1,5 @@
-from tenacity import (
-    retry_if_exception_type,
-    stop_after_attempt,
-    wait_exponential,
-)
-
 import httpx
+from tenacity import retry_if_exception_type, stop_after_attempt, wait_exponential
 
 
 HTTP_RETRY_POLICY = {
