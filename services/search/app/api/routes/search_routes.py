331f791b4af36461e9c47be7bca1b3461ea16c9b
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 11:01:51 +0530

    Feat: Implement ask_question() route: "/ask".

--- services/search/app/api/routes/search_routes.py
+++ services/search/app/api/routes/search_routes.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from services.search.app.schemas.search_schemas import StoreEmbeddingRequest, StoreEmbeddingResponse
 from services.search.app.services import service
+from services.search.app.schemas.search_schemas import StoreEmbeddingRequest, StoreEmbeddingResponse, AskQuestionRequest, AskQuestionResponse
+
 
 search_router = APIRouter()
+
 
 @search_router.post("/internal/store_embedding", response_model=StoreEmbeddingResponse, status_code=status.HTTP_200_OK)
 async def store_embeddings(request: StoreEmbeddingRequest):
@@ -13,6 +15,8 @@ async def store_embeddings(request: StoreEmbeddingRequest):
 
     return response
 
-@search_router.post("/search/ask")
-async def ask_question():
-    ...
+
+@search_router.post("/ask", response_model=AskQuestionResponse, status_code=status.HTTP_200_OK)
+async def ask_question(request: AskQuestionRequest):
+    response = await service.ask_question(request)
+    return response
