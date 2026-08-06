6431846839ce864bfcbbc37818702fd215c87ae7
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 18:33:29 +0530

    Feat: implement retry decorator.

--- shared_lib/retry/decorators.py
+++ shared_lib/retry/decorators.py
@@ -1,1 +1,10 @@
-    
+from tenacity import retry, before_sleep_log
+
+from shared_lib.logger.logger import get_logger
+from shared_lib.retry.policies import HTTP_RETRY_POLICY
+
+logger = get_logger(__name__)
+http_retry = retry(
+    before_sleep=before_sleep_log(logger,logger.warning),
+    **HTTP_RETRY_POLICY,
+    )
