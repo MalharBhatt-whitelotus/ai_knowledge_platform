5370c2b83998d7de42802b3197d5369c86075455
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 16:32:50 +0530

    Refract: Change the ai from openai to genai.

--- services/ai/app/config/setting.py
+++ services/ai/app/config/setting.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 Base_Path = Path(__file__).resolve().parent.parent.parent
 
-class OpenAISetting(BaseSettings):
+class GenAISetting(BaseSettings):
 
-    OPENAI_API_KEY: str
-    OPENAI_MODEL: str
+    GENAI_API_KEY: str
+    GENAI_MODEL: str
 
     model_config = SettingsConfigDict(env_file=Base_Path/".env", extra="ignore")
 
-settings = OpenAISetting()
+settings = GenAISetting()
