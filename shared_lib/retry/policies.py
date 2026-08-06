0a16ed558646e17f31328bbb4e1bc18ed88422c9
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 18:22:11 +0530

    Feat: Implemention of Retry Policies.

---
+++ shared_lib/retry/policies.py
@@ -0,0 +1,25 @@
+from tenacity import (
+    retry_if_exception_type,
+    stop_after_attempt,
+    wait_exponential,
+)
+
+import httpx
+
+
+HTTP_RETRY_POLICY = {
+    "retry": retry_if_exception_type(
+        (
+            httpx.TimeoutException,
+            httpx.ConnectError,
+            httpx.NetworkError,
+        )
+    ),
+    "stop": stop_after_attempt(3),
+    "wait": wait_exponential(
+        multiplier=1,
+        min=1,
+        max=8,
+    ),
+    "reraise": True,
+}
