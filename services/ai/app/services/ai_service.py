590fd2ad900f93658175365f2c1feaca9ba33ba8
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 16:34:20 +0530

    Feat: Check the answer generated from the ai.

--- services/ai/app/services/ai_service.py
+++ services/ai/app/services/ai_service.py
@@ -1,9 +1,9 @@
 from fastapi import status, HTTPException
 
-from services.ai.app.schemas.ai_schemas import AskAIRequest, AskAIResponse
+from services.ai.app.clients.genai_client import GenaiClient
 from services.ai.app.clients.search_client import SearchClient
-from services.ai.app.clients.openai_client import OpenaiClient
 from services.ai.app.services.prompt_builder import PromptBuilder
+from services.ai.app.schemas.ai_schemas import AskAIRequest, AskAIResponse
 
 
 class AIService:
@@ -11,11 +11,11 @@ class AIService:
 
     def __init__(
             self, 
+            genai_client: GenaiClient,
             search_client: SearchClient,
             prompt_builder: PromptBuilder,
-            openai_client: OpenaiClient,
             ) -> None:
-        self.ai_client = openai_client
+        self.ai_client = genai_client
         self.search_client = search_client
         self.prompt_builder = prompt_builder
 
@@ -25,15 +25,18 @@ class AIService:
             search_results = await self.search_client.search(request.question, request.top_k)
             if not search_results:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Search Results not found.")
+            print(f">>> {search_results}")
             
-            prompt = await self.prompt_builder.build(question=request.question, chunks=search_results.chunks,)
+            prompt =  self.prompt_builder.build(question=request.question, chunks=search_results.get("chunks"),)
             if not prompt:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Prompt not found.")
+            print(f">>> {prompt}")
             
             answer = await self.ai_client.generate(prompt=prompt)
             if not answer:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Answer not found.")
-
+            print(f">>> {answer}")
+            
             return AskAIResponse(question=request.question, answer=answer,)
 
         except HTTPException:
