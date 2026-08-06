d7341e1a5c0a26d1c783991097198a5bad366512
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-06 11:09:21 +0530

    Feat: Implement embedding_client dependency injection.

--- services/search/app/services/__init__.py
+++ services/search/app/services/__init__.py
@@ -1,6 +1,8 @@
 from services.search.app.database.chroma import collection
 from services.search.app.services.search_service import SearchService
+from services.search.app.clients.embedding_client import EmbeddingClient
 from services.search.app.repositories.chroma_repository import ChromaRepository
 
+embedding_client = EmbeddingClient()
 repository = ChromaRepository(collection=collection)
-service = SearchService(repo=repository)
+service = SearchService(repo=repository, embedding_client=embedding_client)
