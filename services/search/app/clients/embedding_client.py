aaa5bcde9370374b8335e8bd45c07f7036399884
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-10 11:23:39 +0530

    Feat: Implement retry feature on embedding_client.

--- services/search/app/clients/embedding_client.py
+++ services/search/app/clients/embedding_client.py
@@ -1,5 +1,6 @@
 import httpx
-from fastapi import HTTPException, status
+
+from shared_lib.retry.decorators import http_retry
 
 
 class EmbeddingClient:
@@ -9,19 +10,13 @@ class EmbeddingClient:
         self.base_url = "http://embedding_service:8003"
 
 
+    @http_retry
     async def generate_embeddings(self, question: str):
-        try:
             async with httpx.AsyncClient(timeout=10.0) as client:
                 response = await client.post(
                     f"{self.base_url}/internal/generate_embeddings",
                     json={"chunks": [question]}
                     )
-                if response.status_code != status.HTTP_200_OK:
-                    raise HTTPException(status_code=response.status_code, detail=response.json().get("detail"))
+                response.raise_for_status()
 
                 return response.json()
-        except HTTPException:
-            raise
-
-        except httpx.RequestError as r_exc:
-            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=f"Embedding service unavailable: {str(r_exc)}")
