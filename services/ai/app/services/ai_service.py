bd18a74dd91af58b7d5b5d5728e03e46d53a1fcd
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-10 18:26:36 +0530

    Feat: Implement cache service in ai service.

--- services/ai/app/services/ai_service.py
+++ services/ai/app/services/ai_service.py
@@ -2,7 +2,9 @@ from fastapi import status, HTTPException
 
 from shared_lib.clients.search_client import SearchClient
 
+from services.ai.app.services.cache_key import CacheKey
 from services.ai.app.clients.genai_client import GenaiClient
+from services.ai.app.services.cache_service import CacheService
 from services.ai.app.services.prompt_builder import PromptBuilder
 from services.ai.app.schemas.ai_schemas import AskAIRequest, AskAIResponse
 
@@ -15,10 +17,12 @@ class AIService:
             genai_client: GenaiClient,
             search_client: SearchClient,
             prompt_builder: PromptBuilder,
+            cache_service: CacheService,
             ) -> None:
         self.ai_client = genai_client
         self.search_client = search_client
         self.prompt_builder = prompt_builder
+        self.cache_service = cache_service
 
 
     async def ask(self, request: AskAIRequest):
@@ -27,6 +31,12 @@ class AIService:
             if not search_results:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Search Results not found.")
             print(f">>> {search_results}")
+
+            cache_key = CacheKey.generate(chunks=search_results.get("chunks"), question=request.question)
+
+            cache_answer = self.cache_service.get(key=cache_key)
+            if cache_answer is not None:
+                return AskAIResponse(question=request.question, answer=cache_answer)
             
             prompt =  self.prompt_builder.build(question=request.question, chunks=search_results.get("chunks"),)
             if not prompt:
@@ -37,6 +47,9 @@ class AIService:
             if not answer:
                 raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Answer not found.")
             print(f">>> {answer}")
+
+            await self.cache_service.set(key=cache_key, value=answer)
+            print(">>> Answer is cached...")
             
             return AskAIResponse(question=request.question, answer=answer,)
 
