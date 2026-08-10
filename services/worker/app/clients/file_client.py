dc081a1b5250509cf3e6409a06aacd386359b2b0
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-10 11:24:24 +0530

    Feat: Implement retry feature on file_client.

--- services/worker/app/clients/file_client.py
+++ services/worker/app/clients/file_client.py
@@ -1,8 +1,9 @@
 import httpx
-from fastapi import HTTPException, status
 
 from shared_lib.retry.decorators import http_retry
 from services.worker.app.schemas.file_client_schemas import FileResponse
+
+
 class FileClient:
 
 
@@ -15,75 +16,48 @@ class FileClient:
             self, 
             file_id: str,
             ) -> FileResponse:
-        try: 
             async with httpx.AsyncClient(timeout=10.0) as client:
                 response =  await client.get(
                     f"{self.Base_url}/internal/get_file/{file_id}",
                     )
 
-            if response.status_code != status.HTTP_200_OK:
-                raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+            response.raise_for_status()
             
             return FileResponse.model_validate(response.json())
-        
-        except httpx.RequestError:
-            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="File Service unavailable")
-
-        except HTTPException:
-            raise
 
 
     @http_retry
     async def download_file(self, file_id: str) -> bytes:
-        try:
-            async with httpx.AsyncClient(timeout=10.0) as client:
-                response = await client.get(f"{self.Base_url}/internal/download_file/{file_id}")
+        async with httpx.AsyncClient(timeout=10.0) as client:
+            response = await client.get(
+                f"{self.Base_url}/internal/download_file/{file_id}"
+                )
 
-                if response.status_code != status.HTTP_200_OK:
-                    raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+            response.raise_for_status()
 
-                return response.content
-            
-        except httpx.RequestError:
-            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="File service unavailable.")
-
-        except HTTPException:
-            raise
+        return response.content
 
 
     @http_retry
     async def extract_text(self, file_id: str) -> str:
-        try:
             async with httpx.AsyncClient(timeout=10.0) as client:
-                response = await client.get(f"{self.Base_url}/internal/extract_text/{file_id}")
-                if response.status_code != status.HTTP_200_OK:
-                    raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+                response = await client.get(
+                      f"{self.Base_url}/internal/extract_text/{file_id}"
+                      )
+
+                response.raise_for_status()
 
                 return response.text
-
-        except httpx.RequestError:
-            raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="File Service Unavailable.")
-
-        except HTTPException: 
-            raise
 
 
     @http_retry        
     async def update_status(self, file_id: str, status_update: str) -> str:
         async with httpx.AsyncClient(timeout=10.0) as client:
-            try:
                 respone = await client.post(
                     f"{self.Base_url}/internal/status_update/{file_id}",
                     params={"status":status_update},
                     )
 
-                if respone.status_code != status.HTTP_200_OK:
-                    raise HTTPException(status_code=respone.status_code, detail=respone.json().get("detail"))
+                respone.raise_for_status()
 
                 return respone.json()
-
-            except HTTPException:
-                raise
-
-            except httpx.RequestError as r_exc:
-                raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="File Service unavailable.")
