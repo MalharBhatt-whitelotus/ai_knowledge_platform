0ec51ac4a0bcc50eeade387873426a2f175217ab
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-11 16:53:09 +0530

    Refract: Implement rate limiter on shared middleware.

--- shared_lib/core/middleware.py
+++ shared_lib/core/middleware.py
@@ -1,11 +1,35 @@
 import time
 from fastapi import Request
+from fastapi.responses import JSONResponse
 
-def register_middleware(app, logger):
+def register_middleware(app, logger, rate_limiter=None):
 
     @app.middleware("http")
     async def log_requests(request:Request, call_next):
         start = time.perf_counter()
+
+        if rate_limiter:
+            allowed = await rate_limiter.is_allowed(request)
+            if not allowed:
+                response = JSONResponse(
+                    status_code=429,
+                    content={
+                        "detail": "Too many requests. Please try again later.",
+                    },
+                )
+                duration = (
+                    time.perf_counter() - start
+                    ) * 1000
+                logger.warning(
+                    "%s %s | %s | %.2f ms",
+                    request.method,
+                    request.url.path,
+                    response.status_code,
+                    duration,
+                )
+
+                return response
+            
         response = await call_next(request)
         duration = (time.perf_counter() - start) * 1000
 
