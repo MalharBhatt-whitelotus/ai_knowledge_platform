613a186260088b052cd94cbdf64c5d5359845e02
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-11 14:36:47 +0530

    Feat: Implement celery beats for regular interval cleaning task.

--- services/worker/app/celery_app.py
+++ services/worker/app/celery_app.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from celery.schedules import crontab
 
 from shared_lib.config.settings import settings
 
@@ -9,4 +10,15 @@ celery_app.conf.update(
     result_serializer="json",
     timezone="UTC",
     enable_utc=True,
+    worker_cancel_long_running_tasks_on_connection_loss=True,
+
+    imports= [
+        "services.worker.app.tasks.cleanup_task",
+    ],
 )
+celery_app.conf.beat_schedule = {
+    "run-cleanup-every-10-minutes": {
+        "task": "services.worker.app.tasks.cleanup_task.cleanup_task",
+        "schedule": 600.0,
+    },
+}
