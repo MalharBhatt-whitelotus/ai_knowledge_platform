8e69446a061eecb1b6eb88e689598249e7b0f29b
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-08-18 11:02:53 +0530

    Feat: Implement configuration of gateway service.

---
+++ services/gateway/app/config/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    gateway_host: str = "0.0.0.0"
+    gateway_port: int = 8000
+
+    auth_service_url : str = "http://auth_service:8001"
+    file_service_url : str = "http://file_service:8002"
+    ai_service_url : str = "http://ai_service:8005"
+
+    model_config = SettingsConfigDict(env_file= ".env", env_file_encoding="utf-8", extra="ignore")
+
+settings = Settings()
